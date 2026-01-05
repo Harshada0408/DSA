9c6d00da5968aa8b0b4f01716d21eb7ead91a3f1
Harshada0408 <harshadaborse2125@gmail.com>
2026-01-06 02:32:24 +0530

    simple array

---
+++ basics/basic2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class basic2{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        System.out.println("Enter numbers: ");
+        for(int i = 0; i<arr.length ; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The array elements are:");
+        for(int i = 0; i<arr.length ; i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+}
