6179998989c6cb48ce6636c159e035720feb0812
Harshada0408 <harshadaborse2125@gmail.com>
2026-01-06 02:43:57 +0530

    functions in java

---
+++ basics/basic3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class basic3{
+
+    static int add(int a , int b){
+        return a+b;
+    }
+    public static void main(String [] args){
+        int sum = add(4,2);
+        System.out.println("The sum is: " + sum);
+
+
+    }
+}
