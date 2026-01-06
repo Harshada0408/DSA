79e5e82c7775486b0dcc2ad683f061fe861aba48
Harshada0408 <harshadaborse2125@gmail.com>
2026-01-06 22:41:50 +0530

    Arraylist Implementation

--- basics/basic2.java
+++ basics/basic2.java
@@ -1,20 +1,36 @@
-import java.util.*;
+// import java.util.*;
 
+// public class basic2{
+//     public static void main(String[] args){
+
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a number:");
+//         int n = sc.nextInt();
+//         int [] arr = new int[n];
+//         System.out.println("Enter numbers: ");
+//         for(int i = 0; i<arr.length ; i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         System.out.println("The array elements are:");
+//         for(int i = 0; i<arr.length ; i++){
+//             System.out.println(arr[i]);
+//         }
+        
+//     }
+
+// }
+import java.util.*;
 public class basic2{
     public static void main(String[] args){
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number:");
-        int n = sc.nextInt();
-        int [] arr = new int[n];
-        System.out.println("Enter numbers: ");
-        for(int i = 0; i<arr.length ; i++){
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("The array elements are:");
-        for(int i = 0; i<arr.length ; i++){
-            System.out.println(arr[i]);
-        }
-    }
+        ArrayList<Integer> list = new ArrayList<>();
 
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            list.add(sc.nextInt());
+        }
+
+
+
+    }
 }
--- basics/basic3.java
+++ basics/basic3.java
@@ -1,14 +1,33 @@
 import java.util.*;
-
 public class basic3{
-
-    static int add(int a , int b){
-        return a+b;
-    }
-    public static void main(String [] args){
-        int sum = add(4,2);
-        System.out.println("The sum is: " + sum);
-
-
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println("Enter number of elements:");
+        int n = sc.nextInt();
+        System.out.println("Enter elements:");
+        for(int i=0 ; i<n ;i++){
+            list.add(sc.nextInt());
+        }
+        System.out.println(list);
+        //add elements
+        list.add(100);
+        list.add(2,38);
+        System.out.println(list);
+        
+        //update
+        list.set(1,0);
+        System.out.println(list);
+        
+        //remove
+        list.remove(2);
+        list.remove(Integer.valueOf(4));
+        System.out.println(list);
+        
+        //size
+        System.out.println(list.size());
+        
+        
     }
 }
